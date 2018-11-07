4f3129fe7b39bf3566ff2c42434b0a5c200da60a
jsphkm <jsphkm12@gmail.com>
2018-11-06 22:39:48 -0800

    Improve layout

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var Portfolio = function() {
-  this.settings = {
-    body: document.getElementsByTagName('body')
-  }
-}
+// var Portfolio = function() {
+//   this.settings = {
+//     body: document.getElementsByTagName('body')
+//   }
+// }
 
-Portfolio.prototype = {
+// Portfolio.prototype = {
 
-  init: function() {
-    // settings is available through s now
-    s = this.settings;
-    //this.bindUIActions();
-    this.renderElements();
-  },
+//   init: function() {
+//     // settings is available through s now
+//     s = this.settings;
+//     //this.bindUIActions();
+//     this.renderElements();
+//   },
 
-  renderElements: function() {
-    // Write rendering stuff here
-  },
+//   renderElements: function() {
+//     // Write rendering stuff here
+//   },
 
-  bindUIActions: function() {
-    // Write user actions here
-  }
-}
+//   bindUIActions: function() {
+//     // Write user actions here
+//   }
+// }
 
-function loadmaster(){
-  (new Portfolio).init();
-}
+// function loadmaster(){
+//   (new Portfolio).init();
+// }
 
 window.addEventListener('load', loadmaster());
