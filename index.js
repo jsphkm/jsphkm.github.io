dfa216e60257cbde6f0b9554d5008e7b530fb7a4
jsphkm <jsphkm12@gmail.com>
2019-05-24 20:12:44 -0700

    Refactor names and add javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+const projectsEl = document.getElementById('projects-ul');
+
+const createEl = (element, class) => {
+  const el = document.createElement(element);
+  el.className += class;
+}
+
+const generateProject = () => {
+  
+};
+
+const renderProjects = () => {
+
+};
+
+const master = () => {
+  renderProjects();
+};
+
+window.onload = () => {
+  master();
+};
